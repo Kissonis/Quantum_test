bf0004f4806ea4c924b01c4d377a946a592b166b
Kissonis <60578401+Kissonis@users.noreply.github.com>
2020-09-17 16:31:10 +0300

    Update unet_segmentation_1.py

--- unet_segmentation_1.py
+++ unet_segmentation_1.py
@@ -51,8 +51,8 @@ if __name__ == '__main__':
     os.makedirs(path_saved_models, exist_ok=True)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epoch", type=int, default=10, help="epoch to start training from")
-    parser.add_argument("--n_epochs", type=int, default=1, help="number of epochs of training")
+    parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
+    parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
     parser.add_argument("--dataset_name", type=str, default="data", help="name of the dataset")
     parser.add_argument("--batch_size", type=int, default=4, help="size of the  batches")
     parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
